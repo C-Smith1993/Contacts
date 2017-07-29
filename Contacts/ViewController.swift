63890a18b9cd97876b7ea93e4b1cdedc874731af
C-Smith1993 <chrissmithreds93@hotmail.com>
2017-07-29 11:18:52 +0100

    Able to select a contact and show the contact in more detail.

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -7,19 +7,131 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+struct Contact {
+    let firstName: String
+    let surname: String
+    let contactTitle: String
 }
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+        // UI Connections
+        @IBOutlet weak var tableView: UITableView!
+    
+        // Properties
+        var contacts = [Contact]()
+        var searchURL = "http://demomedia.co.uk/files/contacts.json"
+    
+        // Typealias
+        typealias JSON = [String : AnyObject]
+    
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            callAlamo(url: searchURL)
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+        
+        
+        func callAlamo(url: String){
+            Alamofire.request(url).responseJSON(completionHandler: {
+                response in
+                self.parseData(JSONData: response.data!)
+            })
+        }
+        
+        
+        func parseData(JSONData: Data){
+            do {
+                var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! [JSON]
+                //print(readableJSON)
+
+                    for i in 0..<readableJSON.count{
+                        let item = readableJSON[i]
+                        
+                        let firstnameString = item["firstname"] as! String
+                        
+                        let surnameString = item["surname"] as! String
+                        
+                        let title = item["title"] as! String
+                        
+                        contacts.append(Contact(firstName: firstnameString, surname: surnameString, contactTitle: title))
+                        tableView.reloadData()
+                    }
+                  }catch {
+                print(error)
+            }
+        }
+    
+    
+        
+        
+        
+        // MARK: - TableView Setup
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+            
+            let firstNameLabel = cell?.viewWithTag(1) as! UILabel
+            firstNameLabel.text = contacts[indexPath.row].firstName
+            
+            let surnameLabel = cell?.viewWithTag(2) as! UILabel
+            surnameLabel.text = contacts[indexPath.row].surname
+            
+            return cell!
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return contacts.count
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let indexPath = self.tableView.indexPathForSelectedRow?.row
+        
+        let tvc = segue.destination as! DetailTableViewController
+        
+        tvc.firstname = contacts[indexPath!].firstName
+        tvc.surname = contacts[indexPath!].surname
+        tvc.contactTitle = contacts[indexPath!].contactTitle
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
